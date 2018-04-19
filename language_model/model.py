b4a25e1ca25e6d7be13a7c3e0568137fef5f8147
Tran Duc Thanh <tran@i6.informatik.rwth-aachen.de>
2018-04-19 14:47:17 +0200

    added configuration for LM

--- language_model/model.py
+++ language_model/model.py
@@ -1,1 +1,16 @@
 # TODO: language model as a computation graph
+
+from sockeye.config import Config
+
+class LanguageModelConfig(Config):
+    """
+    Defines the configuration for our stacked RNN language model.
+    """
+    def __init__(self,
+                 embedding_dim: int,
+                 num_rnn_layers: int,
+                 max_seq_len_target: int) -> None:
+        super().__init__()
+        self.embedding_dim = embedding_dim
+        self.num_rnn_layers = num_rnn_layers
+        self.max_seq_len_target = max_seq_len_target
