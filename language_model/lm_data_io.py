7a3f08a8b3468fd4119a425ddd4141feac05d017
Yunsu Kim <kim@cs.rwth-aachen.de>
2018-04-21 18:20:14 +0200

    added data iterators

--- language_model/lm_data_io.py
+++ language_model/lm_data_io.py
@@ -1,1 +1,197 @@
-# TODO: provide data iterators for language model training
+from sockeye import config
+from sockeye import constants as C
+from sockeye.data_io import define_parallel_buckets, get_data_statistics, define_bucket_batch_sizes
+from sockeye import vocab
+
+class LanguageModelDataStatistics(config.Config):
+
+    def __init__(self,
+                 num_sents: int,
+                 num_discarded,
+                 num_tokens,
+                 num_unks,
+                 max_observed_len,
+                 size_vocab,
+                 buckets: List[Tuple[int, int]],
+                 num_sents_per_bucket: List[int],
+                 mean_len_per_bucket: List[Optional[float]]) -> None:
+        super().__init__()
+        self.num_sents = num_sents
+        self.num_discarded = num_discarded
+        self.num_tokens = num_tokens
+        self.num_unks = num_unks
+        self.max_observed_len = max_observed_len
+        self.size_vocab = size_vocab
+        self.buckets = buckets
+        self.num_sents_per_bucket = num_sents_per_bucket
+        self.average_len_per_bucket = mean_len_per_bucket
+
+    def log(self, bucket_batch_sizes: Optional[List[BucketBatchSize]] = None):
+        logger.info("Tokens: %d", self.num_tokens)
+        if self.num_tokens > 0:
+            logger.info("Vocabulary coverage: %.0f%%",
+                        (1 - self.num_unks / self.num_tokens) * 100)
+        logger.info("%d sequences across %d buckets", self.num_sents, len(self.num_sents_per_bucket))
+        logger.info("%d sequences did not fit into buckets and were discarded", self.num_discarded)
+        if bucket_batch_sizes is not None:
+            describe_data_and_buckets(self, bucket_batch_sizes)
+
+class LanguageModelDataInfo(config.Config):
+    """
+    Stores training data information that is not relevant for inference.
+    """
+
+    def __init__(self,
+                 data: str,
+                 vocab: Optional[str],
+                 num_shards: int) -> None:
+        super().__init__()
+        self.data = data
+        self.vocab = vocab
+        self.num_shards = num_shards
+
+class LanguageModelDataConfig(config.Config):
+    """
+    Stores data statistics relevant for inference.
+    """
+
+    def __init__(self,
+                 data_statistics: DataStatistics,
+                 max_seq_len: int) -> None:
+        super().__init__()
+        self.data_statistics = data_statistics
+        self.max_seq_len = max_seq_len
+
+def get_validation_data_iter(data_loader: RawParallelDatasetLoader,
+                             validation_data: str,
+                             buckets: List[Tuple[int, int]],
+                             bucket_batch_sizes: List[BucketBatchSize],
+                             vocab: vocab.Vocab,
+                             max_seq_len: int,
+                             batch_size: int,
+                             fill_up: str) -> 'ParallelSampleIter':
+    """
+    Returns a ParallelSampleIter for the validation data.
+    """
+    logger.info("=================================")
+    logger.info("Creating validation data iterator")
+    logger.info("=================================")
+
+    length_ratio_mean = 1
+    length_ratio_std = 0
+
+    validation_input_sentences = SequenceReader(validation_data, vocab, add_bos=True)
+    validation_output_sentences = SequenceReader(validation_data, vocab, limit=None)
+
+    validation_data_statistics = get_data_statistics([validation_input_sentences],
+                                                     validation_output_sentences,
+                                                     buckets,
+                                                     length_ratio_mean,
+                                                     length_ratio_std,
+                                                     [vocab], vocab)
+
+    validation_data_statistics.log(bucket_batch_sizes)
+
+    validation_data_loaded = data_loader.load([validation_input_sentences],
+                                              validation_output_sentences,
+                                              validation_data_statistics.num_sents_per_bucket).fill_up(bucket_batch_sizes, fill_up)
+
+    return ParallelSampleIter(data=validation_data_loaded,
+                              buckets=buckets,
+                              batch_size=batch_size,
+                              bucket_batch_sizes=bucket_batch_sizes,
+                              num_factors=1)
+
+def get_training_data_iters(train_data: str,
+                            validation_data: str,
+                            vocab: vocab.Vocab,
+                            vocab_path: Optional[str],
+                            batch_size: int,
+                            batch_by_words: bool,
+                            batch_num_devices: int,
+                            fill_up: str,
+                            max_seq_len: int,
+                            bucketing: bool,
+                            bucket_width: int) -> Tuple['BaseParallelSampleIter',
+                                                        'BaseParallelSampleIter',
+                                                        'DataConfig', 'DataInfo']:
+    """
+    Returns data iterators for training and validation data.
+
+    :param train_data: Path to training data.
+    :param validation_data: Path to validation data.
+    :param vocab: Vocabulary.
+    :param vocab_path: Path to vocabulary.
+    :param batch_size: Batch size.
+    :param batch_by_words: Size batches by words rather than sentences.
+    :param batch_num_devices: Number of devices batches will be parallelized across.
+    :param fill_up: Fill-up strategy for buckets.
+    :param max_seq_len: Maximum sequence length.
+    :param bucketing: Whether to use bucketing.
+    :param bucket_width: Size of buckets.
+    :return: Tuple of (training data iterator, validation data iterator, data config, data info).
+    """
+    logger.info("===============================")
+    logger.info("Creating training data iterator")
+    logger.info("===============================")
+
+    # Pass 1: Length ratio is always 1
+    length_ratio_mean = 1
+    length_ratio_std = 0
+
+    # Define buckets
+    buckets = define_parallel_buckets(max_seq_len, max_seq_len, bucket_width,
+                                      length_ratio_mean) if bucketing else [
+        (max_seq_len, max_seq_len)]
+
+    # Input starts from <s>
+    input_sentences = SequenceReader(train_data, vocab, add_bos=True)
+    output_sentences = SequenceReader(train_data, vocab)
+
+    # Pass 2: Get data statistics (for debugging)
+    data_statistics = get_data_statistics([input_sentences], output_sentences, buckets,
+                                          length_ratio_mean, length_ratio_std,
+                                          [vocab], vocab)
+
+    bucket_batch_sizes = define_bucket_batch_sizes(buckets,
+                                                   batch_size,
+                                                   batch_by_words,
+                                                   batch_num_devices,
+                                                   data_statistics.average_len_target_per_bucket)
+
+    data_statistics.log(bucket_batch_sizes)
+
+    # </s> is added here in the output side (labels)
+    data_loader = RawParallelDatasetLoader(buckets=buckets,
+                                           eos_id=vocab[C.EOS_SYMBOL],
+                                           pad_id=C.PAD_ID)
+
+    train_data_loaded = data_loader.load([input_sentences], output_sentences,
+                                         data_statistics.num_sents_per_bucket).fill_up(bucket_batch_sizes, fill_up)
+
+    data_info = LanguageModelDataInfo(data=train_data,
+                                      vocab=vocab_path,
+                                      num_shards=1)
+
+    config_data = LanguageModelDataConfig(data_statistics=data_statistics,
+                                          max_seq_len=max_seq_len)
+
+    train_iter = ParallelSampleIter(data=training_data_loaded,
+                                    buckets=buckets,
+                                    batch_size=batch_size,
+                                    bucket_batch_sizes=bucket_batch_sizes,
+                                    num_factors=1)
+
+    validation_iter = get_validation_data_iter(data_loader=data_loader,
+                                               validation_sources=validation_sources,
+                                               validation_target=validation_target,
+                                               buckets=buckets,
+                                               bucket_batch_sizes=bucket_batch_sizes,
+                                               source_vocabs=source_vocabs,
+                                               target_vocab=target_vocab,
+                                               max_seq_len_source=max_seq_len_source,
+                                               max_seq_len_target=max_seq_len_target,
+                                               batch_size=batch_size,
+                                               fill_up=fill_up)
+
+    return train_iter, validation_iter, config_data, data_info
--- language_model/lm_train.py
+++ language_model/lm_train.py
@@ -1,10 +1,81 @@
+from . import lm_common
+from . import lm_data_io
 from . import lm_model.TrainingLanguageModel
+from sockeye import config
+from sockeye import constants as C
+from sockeye import vocab
+from sockeye import utils
 import argparse
 
 def create_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser()
     # TODO: add parameters
     return parser
+
+def create_data_iters_and_vocabs(args: argparse.Namespace,
+                                 resume_training: bool,
+                                 output_folder: str) -> Tuple['data_io.BaseParallelSampleIter',
+                                                              'data_io.BaseParallelSampleIter',
+                                                              'data_io.DataConfig',
+                                                              List[vocab.Vocab], vocab.Vocab]:
+    """
+    Create the data iterators and the vocabulary.
+
+    :param args: Arguments as returned by argparse.
+    :param resume_training: Whether to resume training.
+    :param output_folder: Output folder.
+    :return: The data iterators (train, validation, config_data) as well as the vocabulary.
+    """
+    max_seq_len = args.max_seq_len
+    num_words = args.num_words
+    word_min_count = args.word_min_count
+    batch_num_devices = 1 if args.use_cpu else sum(-di if di < 0 else 1 for di in args.device_ids)
+    batch_by_words = args.batch_type == C.BATCH_TYPE_WORD
+
+
+    # TODO: option arguments/constants should be well-structured 
+    train_data_error_msg = "Specify a LM training corpus with %s." % ("train-data")
+
+    utils.check_condition(args.train_data is None,
+                          train_data_error_msg)
+
+    if resume_training:
+        # Load the existing vocabs created when starting the training run.
+        lm_vocab = vocab.vocab_from_json(os.path.join(output_folder, lm_common.LANGUAGE_MODEL_PREFIX + "vocab.lm" + JSON_SUFFIX))
+
+        # Recover the vocabulary path from the data info file:
+        data_info = cast(lm_data_io.LanguageModelDataInfo,
+                         Config.load(os.path.join(output_folder, lm_common.LANGUAGE_MODEL_PREFIX + "data.info")))
+        vocab_path = data_info.vocab
+
+    else:
+        # Load or create vocabs
+        vocab_path = args.vocab
+        vocab = vocab.load_or_create_vocab(
+            data=args.train_data,
+            vocab_path=vocab_path,
+            num_words=num_words,
+            word_min_count=word_min_count)
+
+    # No factors for train/validation data
+    train_iter, validation_iter, config_data, data_info = data_io.get_training_data_iters(
+        train_data=os.path.abspath(args.train_data),
+        validation_data=os.path.abspath(args.validation_data),
+        vocab=vocab,
+        vocab_path=vocab_path,
+        batch_size=args.batch_size,
+        batch_by_words=batch_by_words,
+        batch_num_devices=batch_num_devices,
+        fill_up=args.fill_up,
+        max_seq_len=max_seq_len,
+        bucketing=not args.no_bucketing,
+        bucket_width=args.bucket_width)
+
+    data_info_fname = os.path.join(output_folder, lm_common.LANGUAGE_MODEL_PREFIX + "data.info")
+    logger.info("Writing LM data config to '%s'", data_info_fname)
+    data_info.save(data_info_fname)
+
+    return train_iter, validation_iter, config_data, vocab
 
 
 if __name__ == '__main__':
