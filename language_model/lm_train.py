8d23c3396581b047b6a090c1dad2d65d7bd6de10
Yunsu Kim <kim@cs.rwth-aachen.de>
2018-04-30 15:35:51 +0200

    filled lm_create_{model_config,training_model}

--- language_model/lm_train.py
+++ language_model/lm_train.py
@@ -7,6 +7,8 @@ import sys
 from contextlib import ExitStack
 from typing import cast, List, Tuple
 
+import mxnet as mx
+
 from . import lm_arguments
 from . import lm_common
 from . import lm_data_io
@@ -16,10 +18,13 @@ sys.path.append('../')
 
 from sockeye import config
 from sockeye.constants import BATCH_TYPE_WORD
-from sockeye.data_io import DataConfig
+from sockeye.data_io import BaseParallelSampleIter, DataConfig
+from sockeye.encoder import EmbeddingConfig
+from sockeye.loss import LossConfig
+from sockeye.rnn import RNNConfig
 from sockeye.vocab import Vocab, vocab_from_json, load_or_create_vocab
 from sockeye.utils import check_condition
-from sockeye.train import check_resume, determine_context
+from sockeye.train import check_resume, determine_context, gradient_compression_params
 from sockeye.training import EarlyStoppingTrainer
 
 logger = logging.getLogger(__name__)
@@ -27,11 +32,11 @@ logger = logging.getLogger(__name__)
 
 # from sockeye.train
 def lm_create_data_iters_and_vocabs(args: argparse.Namespace,
-                                 resume_training: bool,
-                                 output_folder: str) -> Tuple['data_io.BaseParallelSampleIter',
-                                                              'data_io.BaseParallelSampleIter',
-                                                              'data_io.DataConfig',
-                                                              Vocab]:
+                                    resume_training: bool,
+                                    output_folder: str) -> Tuple['data_io.BaseParallelSampleIter',
+                                                                 'data_io.BaseParallelSampleIter',
+                                                                 'data_io.DataConfig',
+                                                                 Vocab]:
     """
     Create the data iterators and the vocabulary.
 
@@ -45,7 +50,6 @@ def lm_create_data_iters_and_vocabs(args: argparse.Namespace,
     word_min_count = args.word_min_count
     batch_num_devices = 1 if args.use_cpu else sum(-di if di < 0 else 1 for di in args.device_ids)
     batch_by_words = args.batch_type == BATCH_TYPE_WORD
-
 
     # TODO: option arguments/constants should be well-structured
     train_data_error_msg = "Specify a LM training corpus with training and development data."
@@ -82,8 +86,6 @@ def lm_create_data_iters_and_vocabs(args: argparse.Namespace,
         bucketing=not args.no_bucketing,
         bucket_width=args.bucket_width)
 
-    pdb.set_trace() # NOTE: BREAK HERE
-
     data_info_fname = os.path.join(output_folder, lm_common.LM_DATA_INFO)
     logger.info("[LM] Writing LM data config to '%s'", data_info_fname)
     data_info.save(data_info_fname)
@@ -93,57 +95,71 @@ def lm_create_data_iters_and_vocabs(args: argparse.Namespace,
 
 # from sockeye.train
 def lm_create_model_config(args: argparse.Namespace,
-                        data_vocab_size: int,
-                        config_data: DataConfig) -> lm_common.LanguageModelConfig:
-    # TODO: has yet to be modified/checked upon
+                           vocab_size: int,
+                           config_data: DataConfig) -> lm_common.LanguageModelConfig:
     num_embed = args.num_embed
-    embed_dropout_source, embed_dropout_target = args.embed_dropout
-    source_vocab_size, *source_factor_vocab_sizes = source_vocab_sizes
+    embed_dropout = args.embed_dropout
 
-    check_encoder_decoder_args(args)
+    # TODO: adapt check_encoder_decoder_args(args) to LM
+    # check_encoder_decoder_args(args)
 
-    config_conv = None
+    rnn_config = RNNConfig(cell_type=args.rnn_cell_type,
+                           num_hidden=args.rnn_num_hidden,
+                           num_layers=args.num_layers,
+                           dropout_inputs=args.rnn_dropout_inputs,
+                           dropout_states=args.rnn_dropout_states,
+                           dropout_recurrent=args.rnn_dropout_recurrent,
+                           residual=args.rnn_residual_connections,
+                           first_residual_layer=args.rnn_first_residual_layer,
+                           forget_bias=args.rnn_forget_bias)
 
-    config_encoder, encoder_num_hidden = create_encoder_config(args, config_conv)
-    config_decoder = create_decoder_config(args, encoder_num_hidden)
+    # TODO: weight tying
 
-    source_factor_configs = None
-    if len(source_vocab_sizes) > 1:
-        source_factor_configs = [encoder.FactorConfig(size, dim) for size, dim in zip(source_factor_vocab_sizes,
-                                                                                      args.source_factors_num_embed)]
+    config_embed = EmbeddingConfig(vocab_size=vocab_size,
+                                   num_embed=num_embed,
+                                   dropout=embed_dropout)
 
-    config_embed_source = encoder.EmbeddingConfig(vocab_size=source_vocab_size,
-                                                  num_embed=num_embed_source,
-                                                  dropout=embed_dropout_source,
-                                                  factor_configs=source_factor_configs)
+    config_loss = LossConfig(name=args.loss,
+                             vocab_size=vocab_size,
+                             normalization_type=args.loss_normalization_type,
+                             label_smoothing=args.label_smoothing)
 
-    config_embed_target = encoder.EmbeddingConfig(vocab_size=target_vocab_size,
-                                                  num_embed=num_embed_target,
-                                                  dropout=embed_dropout_target)
-
-    config_loss = loss.LossConfig(name=args.loss,
-                                  vocab_size=target_vocab_size,
-                                  normalization_type=args.loss_normalization_type,
-                                  label_smoothing=args.label_smoothing)
-
-    model_config = model.ModelConfig(config_data=config_data,
-                                     vocab_source_size=source_vocab_size,
-                                     vocab_target_size=target_vocab_size,
-                                     config_embed_source=config_embed_source,
-                                     config_embed_target=config_embed_target,
-                                     config_encoder=config_encoder,
-                                     config_decoder=config_decoder,
-                                     config_loss=config_loss,
-                                     weight_tying=args.weight_tying,
-                                     weight_tying_type=args.weight_tying_type if args.weight_tying else None,
-                                     weight_normalization=args.weight_normalization)
+    model_config = lm_common.LanguageModelConfig(config_data=config_data,
+                                                 vocab_size=vocab_size,
+                                                 num_embed=num_embed,
+                                                 rnn_config=rnn_config,
+                                                 config_embed=config_embed,
+                                                 config_loss=config_loss,
+                                                 weight_tying=args.weight_tying)
     return model_config
 
 
-def lm_create_training_model():
-    # TODO: implementation
-    # TODO: check consistency with TrainingModel class (sockeye)
-    pass
+def lm_create_training_model(config: lm_common.LanguageModelConfig,
+                             context: List[mx.Context],
+                             output_dir: str,
+                             train_iter: BaseParallelSampleIter,
+                             args: argparse.Namespace) -> lm_model.TrainingLanguageModel:
+    """
+    Create a training model and load the parameters from disk if needed.
+
+    :param config: The configuration for the model.
+    :param context: The context(s) to run on.
+    :param output_dir: Output folder.
+    :param train_iter: The training data iterator.
+    :param args: Arguments as returned by argparse.
+    :return: The training model.
+    """
+    training_model = lm_model.TrainingLanguageModel(config=config,
+                                                    context=context,
+                                                    output_dir=output_dir,
+                                                    provide_data=train_iter.provide_data,
+                                                    provide_label=train_iter.provide_label,
+                                                    default_bucket_key=train_iter.default_bucket_key,
+                                                    bucketing=not args.no_bucketing,
+                                                    gradient_compression_params=gradient_compression_params(args),
+                                                    fixed_param_names=[])
+
+    return training_model
 
 
 if __name__ == '__main__':
@@ -155,26 +171,28 @@ if __name__ == '__main__':
     with ExitStack() as exit_stack:
         context = determine_context(args, exit_stack)
 
-        train_iter, eval_iter, config_data, data_vocabs = lm_create_data_iters_and_vocabs(
-                                                                    args=args,
-                                                                    resume_training=resume_training,
-                                                                    output_folder=output_folder)
+        train_iter, eval_iter, config_data, data_vocab = lm_create_data_iters_and_vocabs(
+            args=args,
+            resume_training=resume_training,
+            output_folder=output_folder)
 
-        data_vocab_size = len(data_vocabs)
+        data_vocab_size = len(data_vocab)
         logger.info('[LM] Vocabulary size: %s', data_vocab_size)
 
         lm_model_config = lm_create_model_config(args, data_vocab_size, config_data)
         lm_model_config.freeze()
 
         lm_training_model = lm_create_training_model(config=lm_model_config,
-                                 context=context,
-                                 output_dir=output_folder,
-                                 train_iter=train_iter,
-                                 args=args)
+                                                     context=context,
+                                                     output_dir=output_folder,
+                                                     train_iter=train_iter,
+                                                     args=args)
+
+        pdb.set_trace() # NOTE: BREAK HERE
 
         lm_trainer = sockeye.training.EarlyStoppingTrainer(model=lm_training_model,
-                                              optimizer_config=create_optimizer_config(args,ddata_vocab_size),
-                                              max_params_files_to_keep=args.keep_last_params)
+                                                           optimizer_config=create_optimizer_config(args,ddata_vocab_size),
+                                                           max_params_files_to_keep=args.keep_last_params)
         lm_trainer.fit(train_iter=train_iter,
                        validation_iter=eval_iter,
                        early_stopping_metric=args.optimized_metric,
