e42efe65b5f9eec50a367a58bddba3bde74d0b51
Tran Duc Thanh <tran@i6.informatik.rwth-aachen.de>
2018-05-09 16:21:25 +0200

    removed unnecessary stuff

--- language_model/lm_inference_test.py
+++ language_model/lm_inference_test.py
@@ -23,11 +23,3 @@ def test_loading_models():
                                                     batch_size=BATCH_SIZE,
                                                     model_folder=SWITCHBOARD_PATH,
                                                     decoder_return_logit_inputs=False)
-
-def test_inference_softmax():
-    model_softmax_output = lm_inference.load_models(context=CONTEXT,
-                                                    max_output_len=MAX_OUTPUT_LEN,
-                                                    batch_size=BATCH_SIZE,
-                                                    model_folder=SWITCHBOARD_PATH,
-                                                    decoder_return_logit_inputs=False)
-    model.
