33bb1caf29db948dd7fb7670b5fbd83184ca67e3
Tran Duc Thanh <tran@i6.informatik.rwth-aachen.de>
2018-05-11 13:20:29 +0200

    added hidden state test

--- language_model/lm_inference_test.py
+++ language_model/lm_inference_test.py
@@ -7,6 +7,7 @@ from . import lm_inference
 
 SWITCHBOARD_PATH = '/work/smt2/tran/work/language_model_sockeye/models/switchboard/output.lr-00003'
 VOC_SIZE = 1029
+HIDDEN_UNITS = 1024
 
 MAX_OUTPUT_LEN = 100
 BATCH_SIZE = 1
@@ -38,20 +39,46 @@ def test_inference_softmax():
                                      batch_size=BATCH_SIZE,
                                      model_folder=SWITCHBOARD_PATH)
 
-    zero_vector = mx.nd.zeros((1,1024)) # same dimensionality as hidden units used during training, i.e. 1024
+    zero_vector = mx.nd.ones((1,HIDDEN_UNITS)) # same dimensionality as hidden units used during training, i.e. 1024
     lm_state = lm_inference.ModelState([zero_vector])
 
-    def test_seq(sentence, step):
+    def sentence_assertions_softmax(inferer, sentence, step):
         out, updated_lm_states = inferer.decode_step(lm_states=lm_state,
-                                                     sentence=sentence,
-                                                     step=step)
+                                                        sentence=sentence,
+                                                        step=step)
         assert out.shape[1] == VOC_SIZE
         assert out.sum().asscalar() == pytest.approx(1.0, abs=1e-1) # out.sum() is an mx.nd.array - cast to int?
 
-        print(out.max())
-        print(mx.ndarray.argmax(out, axis=1))
-        print(updated_lm_states.states) # empty?? we did not update correctly
+        print("Sentence: {}".format(sentence))
+        print("softmax maximum value: {}".format(out.max()))
+        print("softmax argmax: {}".format(mx.ndarray.argmax(out, axis=1)))
+        print("updated lm_states: {}".format(updated_lm_states.states))# empty?? we did not update correctly
 
-    test_seq(SEQUENCE_BOS, step=1) # 'yeah' is argmax softmax output
-    test_seq(SEQUENCE_ONE, step=2) # ''
-    test_seq(SEQUENCE_TWO, step=3) # 'mall'
+    sentence_assertions_softmax(inferer, SEQUENCE_BOS, step=1) # 'yeah' is argmax softmax output
+    sentence_assertions_softmax(inferer, SEQUENCE_ONE, step=2) # ''
+    sentence_assertions_softmax(inferer, SEQUENCE_TWO, step=3) # 'mall'
+
+
+def test_inference_hiddenstate():
+    inferer = lm_inference.LMInferer(context=CONTEXT,
+                                     max_output_len=MAX_OUTPUT_LEN,
+                                     batch_size=BATCH_SIZE,
+                                     model_folder=SWITCHBOARD_PATH,
+                                     decoder_return_logit_inputs=True)
+
+    zero_vector = mx.nd.ones((1, HIDDEN_UNITS)) # same dimensionality as hidden units used during training, i.e. 1024
+    lm_state = lm_inference.ModelState([zero_vector])
+
+    def sentence_assertions_hiddenstate(inferer, sentence, step):
+        out, updated_lm_states = inferer.decode_step(lm_states=lm_state,
+                                                        sentence=sentence,
+                                                        step=step)
+        assert out.shape[1] == HIDDEN_UNITS
+
+        print("Sentence: {}".format(sentence))
+        print("output {}".format(out))
+        print("updated lm_states: {}".format(updated_lm_states.states)) # empty?? we did not update correctly
+
+    sentence_assertions_hiddenstate(inferer, SEQUENCE_BOS, step=1) # 'yeah' is argmax softmax output
+    sentence_assertions_hiddenstate(inferer, SEQUENCE_ONE, step=2) # ''
+    sentence_assertions_hiddenstate(inferer, SEQUENCE_TWO, step=3) # 'mall'
